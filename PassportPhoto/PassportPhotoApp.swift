4d5da92cc212bde52873eab14efc845ccfb794a6
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-04 18:59:10 +0300

    Initial Commit

---
+++ PassportPhoto/PassportPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassportPhotoApp.swift
+//  PassportPhoto
+//
+//  Created by Mehmet Tekin on 4.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PassportPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
